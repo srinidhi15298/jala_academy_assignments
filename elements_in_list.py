0447b52abb75b4adeee1ce4d09e7c976282926d6
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 12:46:39 +0530

    Add files via upload

---
+++ elements_in_list.py
@@ -0,0 +1,6 @@
+# 17. Write a method to verify if the array contains two specified elements(12,23)
+l1=[78,12,23,45,67]
+if 12 in l1 and 23 in l1:
+    print("list has specified elements")
+else:
+    print("list doesn't have specified elements")
