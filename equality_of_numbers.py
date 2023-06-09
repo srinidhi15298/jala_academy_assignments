7a6070f1dc2a5c6040e6b0ca99ed6729bdc40033
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 10:40:17 +0530

    Add files via upload

---
+++ equality_of_numbers.py
@@ -0,0 +1,5 @@
+# 3. Write a program to find the two numbers equal or not
+a = 5
+b = 6
+
+print("a equals b is ", a == b)
---
+++ increment_decrement.py
@@ -0,0 +1,11 @@
+#2. Write a method for increment and decrement operators(++, --)
+
+a = 5
+b = 6
+
+print("value of a before increment a=", a)
+print("value of b before decrement b=", b)
+# after increment of a and decrement of b
+print("increment value of a=", ++a)
+print("decrement value of b=", --b)
+
