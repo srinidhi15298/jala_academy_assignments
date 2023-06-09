c26792710d6729bfa0f202617d43c7a7b6db8a7e
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 11:29:14 +0530

    Add files via upload

---
+++ add_integers.py
@@ -0,0 +1,7 @@
+# 1. Write a function to add integer values of an array
+#no arrays in python we can use lists
+list =[1,2,3,4]
+sum=0
+for i in range(0,len(list)):
+    sum=sum+list[i]
+print("added values of list is ",sum)
