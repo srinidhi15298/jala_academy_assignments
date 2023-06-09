dec6bb1b0f61d2d495bfb0d1b91a1ff85c198ee6
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 12:44:00 +0530

    Add files via upload

---
+++ large_small.py
@@ -0,0 +1,4 @@
+# 16. Write a function to get the difference of largest and smallest value
+l1=[56,78,90,12]
+diff=max(set(l1))-min(set(l1))
+print("difference between max and min of list is :",diff)
