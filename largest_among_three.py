a0f0d459e2973e4ec53ccab278a023946c384d32
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 11:04:05 +0530

    Add files via upload

---
+++ largest_among_three.py
@@ -0,0 +1,9 @@
+# 5. Write a program to print largest number among three numbers.
+a, b, c = 6, 5, 3
+if a > b and a > c:
+    print("largest number among 6,5,3 is:", a)
+elif b > a and b > c:
+    print("largest number among 6,5,3 is:", b)
+else:
+    print("largest number among 6,5,3 is:", c)
+
