4e660f0c0677df9cd0c9aa28f73d9e1a250b7f4d
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 12:49:16 +0530

    Add files via upload

---
+++ remove_duplicates_list.py
@@ -0,0 +1,5 @@
+# 18. Write a program to remove the duplicate elements and return the new array
+l1=[1,6,1,9,0,8,9]
+print("list before removal of duplicates:",l1)
+l1=list(set(l1))
+print("list after removal of duplicates:",l1)
