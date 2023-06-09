6917c92070eee65c008db98ca1e361a82827de12
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 10:40:47 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,15 @@
+# 1. Write a function for arithmetic operators(+,-,*,/)
+
+# addition
+a = 2+3
+print("2+3=", a)
+# subtraction
+s = 6-3
+print("6-3= ", s)
+# multiplication
+m = 2*3
+print("2*3=", m)
+# division
+d = 6/3
+print("6/3=", d)
+
