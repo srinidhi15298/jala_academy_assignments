f84b87b717c099ba1c32a72a73a0ac361a4a3d35
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 11:22:06 +0530

    Add files via upload

---
+++ switch_even_odd.py
@@ -0,0 +1,8 @@
+# 11. Program to check whether a number is EVEN or ODD using switch
+
+#there is no built in switch case in python
+num = int(input("enter number to check even or odd: "))
+if num%2==0:
+    print("number is even")
+else:
+    print("number is odd")
