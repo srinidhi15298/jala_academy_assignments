474f55eaa26e99c6011818eb7895d686fc687724
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 11:48:13 +0530

    Add files via upload

---
+++ min_max_list.py
@@ -0,0 +1,5 @@
+# 8. Write a function to find the minimum and maximum value of an array
+
+list=[7,0,9,3]
+print("minimium value of list is ",min(list))
+print("maximum value of list is ",max(list))
