abf20ccdd243fa2b61a143433312dfb866923788
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 11:45:52 +0530

    Add files via upload

---
+++ insert_at_specific_position.py
@@ -0,0 +1,5 @@
+# 7. Write a function to insert an element at a specific position in the array4
+list =[1,7,8]
+print("list before inserting ", list)
+list.insert(0,9)
+print("list after inserting at index 0: ", list)
