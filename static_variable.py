28854f9ab4cc8fb3c4d73d9a51588926db798f00
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 12:56:17 +0530

    Add files via upload

---
+++ static_variable.py
@@ -0,0 +1,7 @@
+#1. Define a static variable and access that through a class
+
+# there are no static variables in python class variables are there
+class MyClass:
+    static_var = 0
+    print("static variable is:", static_var)
+
