1a04be27e303e32fec5b284010a251911050deb5
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 11:11:39 +0530

    Add files via upload

---
+++ do_while.py
@@ -0,0 +1,8 @@
+# 7. Write a program to print 1 to 10 using the do-while loop statement.
+
+# there is no do while loop in python but while is there
+
+a = 1
+while a < 11:
+    print(a)
+    a += 1
