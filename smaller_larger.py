9a979340c0fe9addae631ea76b7eeff3701ef61c
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 10:48:29 +0530

    Add files via upload

---
+++ smaller_larger.py
@@ -0,0 +1,7 @@
+# 5. Print the smaller and larger number
+a = 5
+b = 4
+if a < b:
+    print("smaller number is a=", a, "and larger number is b=", b)
+else:
+    print("smaller number is b=", b, "and larger number is a=", a)
