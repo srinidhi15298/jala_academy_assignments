2343f91a483c4a6e5cac270e74cb67d63052ac61
srinidhi15298 <32964751+srinidhi15298@users.noreply.github.com>
2023-06-09 10:57:50 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,8 @@
+# 4. Write a program to print the odd and even numbers.
+
+# even and odd in range 20
+for i in range(0, 21):
+    if i % 2 == 0:
+        print(i, " is even")
+    else:
+        print(i, "is odd")
